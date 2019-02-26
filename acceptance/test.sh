3f14d5e54c53e4102e809ce0d1ae833b11edcee9
Kevin Edwards <kedwards@kevinedwards.ca>
2019-02-26 00:27:43 -0700

    more acceptance testing

--- acceptance/test.sh
+++ acceptance/test.sh
@@ -1,7 +1,5 @@
-#!/bin/bash
+#!/bin/sh
 
-CALCULATOR_PORT=$(docker-compose port calculator 8080 | cut -d: -f2)
+test $(curl -s localhost:8080/sum?a=1\&b=2) -eq 3 
 
-sleep 30
-test $(curl calculator:8080/sum?a=1\&b=2) -eq 3
 #./gradlew acceptanceTest -Dcalculator.url=http://localhost:8080
