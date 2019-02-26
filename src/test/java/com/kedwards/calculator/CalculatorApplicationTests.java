e2581630d1ebabe1b004cef4ab85e8583eeefe17
Kevin Edwards <kedwards@kevinedwards.ca>
2019-02-25 23:35:35 -0700

    fixed acceptance tests.

--- src/test/java/com/kedwards/calculator/CalculatorApplicationTests.java
+++ src/test/java/com/kedwards/calculator/CalculatorApplicationTests.java
@@ -10,7 +10,9 @@ import org.springframework.test.context.junit4.SpringRunner;
  */
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class CalculatorApplicationTests {
+public class ApplicationTest {
 
-
+    @Test
+    public void contextLoads() throws Exception {
+    }
 }
