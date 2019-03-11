09a7aee91ba36f02ea330d2eacd6acbc36de90f0
Kevin Edwards <kedwards@kevinedwards.ca>
2019-03-11 00:57:45 -0600

    testing

--- acceptance_test.sh
+++ acceptance_test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-test $(curl -s localhost:8765/sum?a=1\&b=2) -eq 3
+test $(curl -s localhost:8080/sum?a=1\&b=2) -eq 3
 
 #./gradlew acceptanceTest -Dcalculator.url=http://localhost:8080
