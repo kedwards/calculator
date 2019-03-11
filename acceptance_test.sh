f7f6e6b897f353d7d5488ba895e6069cfd8a2b9b
Kevin Edwards <kedwards@kevinedwards.ca>
2019-03-10 23:18:56 -0600

    test 7

--- acceptance_test.sh
+++ acceptance_test.sh
@@ -1,12 +1,5 @@
-#!/bin/sh
+#!/bin/bash
 
-VAL=$(curl -s localhost:8765/sum?a=1\&b=2)
-
-if [ ${VAL} == 3 ]
-then
-    exit 0
-else
-    exit 1
-fi
+test $(curl -s localhost:8765/sum?a=1\&b=2) -eq 3
 
 #./gradlew acceptanceTest -Dcalculator.url=http://localhost:8080
