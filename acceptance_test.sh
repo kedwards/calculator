214439794010303ab8387e6501e4c8677752fa3c
Kevin Edwards <kedwards@kevinedwards.ca>
2019-03-11 09:29:32 -0600

    echos

--- acceptance_test.sh
+++ acceptance_test.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-test $(curl -s localhost:8080/sum?a=1\&b=2) -eq 3
+CALCULATOR_PORT=$(docker-compose port calculator 8080 | cut -d: -f2)
+
+echo "Calculaor Port is: ${CALCULATOR_PORT}"
+
+echo "test $(curl -s ${CALCULATOR_PORT}:8080/sum?a=1\&b=2) -eq 3"
+
+test $(curl -s ${CALCULATOR_PORT}:8080/sum?a=1\&b=2) -eq 3
 
 #./gradlew acceptanceTest -Dcalculator.url=http://localhost:8080
