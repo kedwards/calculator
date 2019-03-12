f301d78aeeff217f38629c3809f2e8a50f80c53d
Kevin Edwards <kedwards@kevinedwards.ca>
2019-03-11 21:51:23 -0600

    Major update

--- acceptance/test.sh
+++ acceptance/test.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 sleep 30
-test $(curl calculator:8080/sum?a=1\&b=2) -eq 3
 
-#./gradlew acceptanceTest -Dcalculator.url=http://localhost:8080
+test $(curl calculator:8080/sum?a=1\&b=2) -eq 3
--- acceptance_test.sh
+++ acceptance_test.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+
 CALCULATOR_PORT=$(docker-compose port calculator 8080 | cut -d: -f2)
-test $(curl localhost:$CALCULATOR_PORT/sum?a=1\&b=2) -eq 3
+
+test $(curl localhost:$CALCULATOR_PORT/sum?a=1\&b=2) -eq 3 || exit 1
+
+./gradlew acceptanceTest -Dcalculator.url=http://localhost:${CALCULATOR_PORT}
